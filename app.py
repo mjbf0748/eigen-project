b0ab1e3b0e3a58fbf6a383aabd9fa9a0c4c50e26
mjbf0748 <mjbf0748@gmail.com>
2020-08-10 13:43:25 -0400

    Added comments and cleanup

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask import Flask, jsonify, request 
+from flask_cors import CORS 
 import sys
-sys.path.insert(1, './routes')
+sys.path.insert(1, './routes') # point to routes folder
 
 import router
+
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api": {"origins": "*"}})
 
+# Route to find important words
 @app.route("/api", methods=['POST'])
-def home():
+def findWords():
     files = request.files
     res = router.extractWords(files)
     return jsonify(res)
